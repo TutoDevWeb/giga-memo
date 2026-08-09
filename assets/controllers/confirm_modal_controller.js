0aac29b4345edcd22b7ea0a04bd0ac2076ac882c
TutoDevWeb <TutoDevWeb@outlook.fr>
2026-08-09 08:23:33 +0200

    Toujours sur la modale

--- assets/controllers/confirm_modal_controller.js
+++ assets/controllers/confirm_modal_controller.js
@@ -1,18 +1,14 @@
 import { Controller } from '@hotwired/stimulus';
 
 // Ce controleur intervient dans l'affichage d'une modale bootstrap utilisée pour confirmer les actions supprimer
+// Ce controleur récupère l'action(url) et le token depuis le bouton qui ouvre la modale.
+// Ensuite il cible le formulaire qui contient le bouton de la modale qui va faire la suppression.
+// Il lui passe l'action et le token.
 // Les boutons qui déclenchent la modale sont dans les fichiers _delete_form.twig.html de chaque CRUD
 // La modale est dans le fichier _partials/_delete_modal.twig.html
 // La modale est une modale boostrap. 
 // C'est bootstrap qui déclenche l'affichage de la modale lorsque l'utilisateur appui sur un bouton.
-// Il faut donc implanter sur le bouton les info nécessaires à l'action de suppression depuis la modale.
-// A cette fin, sur le bouton sont implantés le 
-// La route du controleur de suppression data-action="{{ path('app_couples_delete', {'id_faq': faq.id, 'id_couple': couple.id}) }}" 
-// et le token 
-// data-token="{{ csrf_token('delete' ~ couple.id) }}"
-//
-// Ensuite action et token sont passer au formulaire contenu dans la modale
-// Dans la modale le bouton a été remplacer par un form.
+
 // <form method="post" action="" data-confirm-modal-target="form">
 // 	<input type="hidden" name="_token" value="" data-confirm-modal-target="token">
 // 	<button type="submit" class="btn btn-delete-confirm">Supprimer définitivement</button>
@@ -28,14 +24,14 @@ export default class extends Controller {
 
         if (!button) return;
 
-        // Récupération des données passées sur le bouton
+        // Récupération des données présentes sur le bouton qui ouvre la modale
         const action = button.dataset.action;
         const token = button.dataset.token;
 
         console.log('action => ', action);
         console.log('token => ', token);
 
-        // On passe ici action et token à la modale
+        // On passe action et token à la modale
         if (action && this.hasFormTarget) {
             this.formTarget.action = action;
         }
