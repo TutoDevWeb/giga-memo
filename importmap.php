33ca69858aa2b7e707d9c8b50ea64b41b3110821
TutoDevWeb <TutoDevWeb@outlook.fr>
2026-08-22 08:25:02 +0200

    On passe par l'importmap pour charger bootstrap et bootsrap icons.(CC)

--- importmap.php
+++ importmap.php
@@ -25,4 +25,18 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
+    'bootstrap' => [
+        'version' => '5.3.8',
+    ],
+    'bootstrap-icons/font/bootstrap-icons.css' => [
+        'version' => '1.13.1',
+        'type' => 'css',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.8',
+        'type' => 'css',
+    ],
 ];
