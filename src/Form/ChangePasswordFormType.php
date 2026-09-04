228125ef79b47a980418715737be88b899edfef0
TutoDevWeb <TutoDevWeb@outlook.fr>
2026-09-04 09:26:44 +0200

    On a corriger quelque messages qui étaient en Anglais.

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -27,28 +27,27 @@ class ChangePasswordFormType extends AbstractType
                 'first_options' => [
                     'constraints' => [
                         new NotBlank(
-                            message: 'Please enter a password',
+                            message: 'Merci de saisir un mot de passe',
                         ),
                         new Length(
-                            min: 12,
-                            minMessage: 'Your password should be at least {{ limit }} characters',
+                            min: 6,
+                            minMessage: 'Votre mot de passe doit contenir au minimum {{ limit }} caractères',
                             // max length allowed by Symfony for security reasons
                             max: 4096,
                         ),
                         new PasswordStrength(),
                         new NotCompromisedPassword(),
                     ],
-                    'label' => 'New password',
+                    'label' => 'Nouveau mot de passe',
                 ],
                 'second_options' => [
-                    'label' => 'Repeat Password',
+                    'label' => 'Répétez le mot de passe',
                 ],
-                'invalid_message' => 'The password fields must match.',
+                'invalid_message' => 'Les deux mots de passe doivent être identiques.',
                 // Instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
